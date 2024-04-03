8ffa08c03819f97e9bb03d100420c1ecdb5fd23a
mderdun <mderdun@gmail.com>
2024-04-03 15:38:12 +0100

    Vol 3 drafted

---
+++ src/routes/works/vol3/+page.svelte
@@ -0,0 +1,101 @@
+<script>
+    import WorksHead from "$lib/components/WorksHead.svelte";
+</script>
+<div id="body" class="body text-white">
+    <WorksHead title="my dear miracle in the winery" />
+    <div id="container" class="w-screen flex items-center justify-center">
+    <div id="poem" class="flex flex-col text-xl">
+        <div id="epigraph" class="text-sm flex flex-col text-right opacity-90">
+            <p>you are the one who makes the wine</p>
+            <p>you are the one who drinks the wine</p>
+            <p>you are the one who is the wine</p>
+            <p>you are the one who is the winery</p>
+            <p>you are the one who is the miracle</p>
+            <p>you are the one who is the dear</p>
+            <p>thank you KONSTANTY ILDEFONS GAŁCZYŃSKI</p>
+        </div>
+        <div id="stanza1" class="stanza">
+            <p class="dear">truth: he has a car we can't afford</p>
+            <p class="dear">my dear</p>
+            <p class="dear">but look: only fools are in a hurry—how much more dignified it is to walk</p>
+            <p class="dear">my dear</p>
+        </div>
+        <div id="stanza2" class="stanza">
+            <p class="dear">there are also no funds for london</p>
+            <p class="alt">two skippers from the london docks found it one year a forgotten wine barrel they took off the lid and both of them stru­chle­li; z becz­ki oso­ba sze­ścio­skrzy­die­go se­ra­fi­na! serafin was walking up the stairs ja­ko­by lam­pa słod­ka and it smelled</p>
+            <p class="dear">my dear</p>
+            <p class="alt">but it was decreasing</p>
+            <p class="dear">but why london? london—burrow: in “paris-soir” I read yesterday</p>
+            <p class="alt">and the poor kippers they scratched their heads</p>
+            <p class="dear">that marquis q</p>
+            <p class="alt">that character is not a local</p>
+        </div>
+        <div id="stanza3" class="stanza">
+            <p class="dear">that is</p>
+            <p class="alt">just the wind with a furious storm tore london like a tree—a visible act of god; the king hid in the cupboard</p>
+            <p class="dear">a baronet</p>
+            <p class="alt">they were shaking under the beds go­li­bro­dy i astro­no­my</p>
+            <p class="dear">he poisoned his wife with dog noodles... bloody hell with this london</p>
+            <p class="alt2">so this sad london our seraphim is wandering</p>
+            <p class="dear">my dear !</p>
+        </div>
+        <div id="stanza4" class="stanza">
+            <p class="alt2">homeless</p>
+            <p class="dear">for caviar from the kremlin</p>
+            <p class="alt2">the rain poured down on him</p>
+            <p class="dear">for sturgeon we don't have any either</p>
+            <p class="alt2">a policeman will shout from the bridge:</p>
+            <p class="dear">my dear</p>
+            <p class="alt2">"who are you?"—and he just did</p>
+            <p class="dear">but what good is the kremlin</p>
+            <p class="alt2">"i am the pole</p>
+            <p class="dear">when I have your hips?</p>
+            <p class="alt2">jam po­lak"</p>
+            <p class="dear">the hips are golden</p>
+            <p class="alt2">"that's beautiful</p>
+            <p class="dear">and the night is blue</p>
+            <p class="alt2">my winged one</p>
+            <p class="dear">and we also have a glass of wine and an account in heaven with a seraphim</p>
+            <p class="alt2">please go to the embassy there's everything for poles</p>
+            <p class="dear">so whistle for the car</p>
+            <p class="alt2">good english "cop" serafin explained: page such and such</p>
+        </div>
+        <div id="stanza5" class="stanza pb-10">
+            <p class="dear">kremlin and london</p>
+            <p class="alt2">there's an angel in the embassy he cried over the whole stool</p>
+            <p class="dear">my dear</p>
+            <p class="alt2">form</p>
+            <p class="dear">&nbsp;</p>
+            <p class="alt2">stamp and pad</p>
+            <p class="dear">&nbsp;</p>
+            <p class="alt2">he borrowed from grief pln 340 and disappeared like a cloud</p>
+        </div>
+    </div>
+    </div>
+</div>
+
+<style>
+    .body {
+        display: block;
+        background-image: url("../../../lib/imgs/beach_THREEwater_ani.gif");
+        background-size: cover;
+    }
+    .stanza {
+        margin-top: 2.5em;
+    }
+    #poem {
+        max-width: 550px
+    }
+    .alt {
+        margin-left: 2em;
+        margin-top: -0.8em;
+    }
+    .alt2{
+        margin-left: -3.94em;
+        margin-top: -0.8em;
+    }
+    p {
+        padding-left: 1em;
+        text-indent: -1em;
+    }
+</style>
